a340e93e183b7a27e58a82646c186c2b7925511c
Ronaldo <robarros@gmail.com>
2018-12-29 15:44:46 +0000

    chane deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,1 @@
-sed -i "s/IMAGE1/${IMAGE_NAME}:${IMAGE_VERSION}/" Deployment.yaml
-cat Deployment.yaml
-# kubectl apply -f Deployment.yaml
+sed -i "s/IMAGE1/${DOCKERHUB_USER}\/${IMAGE_NAME}:${IMAGE_VERSION}/g" Deployment.yaml
