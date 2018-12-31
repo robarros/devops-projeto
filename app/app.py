3dc53fe31beb42c020422dbbf319e3d86cc257c5
Ronaldo <robarros@gmail.com>
2018-12-31 22:01:39 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h3>Feliz Ano Novo</h3>" \
+    html = "<h3>Feliz 2019</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" 
 
     return html.format(hostname=socket.gethostname())
