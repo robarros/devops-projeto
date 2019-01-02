14c9078316d3d642be19653a21dbcbef9186d235
robarros <robarros@gmail.com>
2019-01-02 11:16:07 -0200

    terraform

---
+++ terraform/main.tf
@@ -0,0 +1,46 @@
+provider "vsphere" {
+  user           = "root"
+  password       = "teste.1"
+  vsphere_server = "10.0.0.250"
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "dc1"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "datastore1"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "cluster1/Resources"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-test"
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+
+  num_cpus = 1
+  memory   = 512
+  guest_id = "centos7_64Guest"
+  wait_for_guest_net_timeout = "1"
+  force_power_off  = true
+
+  network_interface {
+    network_id = "${data.vsphere_network.network.id}"
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
