7683b51e4b633d16a750e328599adb004a31a906
Ronaldo Barros <robarros@gmail.com>
2019-01-03 13:41:11 +0000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 IMAGEM=$(cat $WORKSPACE/IMAGE_FULL.tag)
-sed -i.bkp "s|IMAGEM1|${IMAGE_FULL}|g" $WORKSPACE/kubernetes/Deployment.yaml
+sed -i.bkp "s|IMAGEM1|${IMAGEM}|g" $WORKSPACE/kubernetes/Deployment.yaml
