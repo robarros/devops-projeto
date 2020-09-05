1ad0f9aa11aa7d572cd01b05136ffd2f8995e374
robarros <20506531+robarros@users.noreply.github.com>
2020-09-04 23:04:09 -0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 IMAGEM=$(cat $WORKSPACE/IMAGE_FULL.tag)
-sed -i.bkp "s|IMAGEM1|${IMAGEM}|g" $WORKSPACE/kubernetes/Deployment.yaml
+sed -i.backup "s|IMAGEM1|${IMAGEM}|g" $WORKSPACE/kubernetes/Deployment.yaml
